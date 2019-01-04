bc3ce3a117c59db403d733aafd4e5ff7d1e432ba
Kiana Smith <kiana@gmail.com>
2019-01-04 18:30:06 -0500

    techJobs-OO Complete

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,10 +1,12 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Job;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -24,7 +26,7 @@ public class JobController {
     public String index(Model model, int id) {
 
         // TODO #1 - get the Job with the given ID and pass it into the view
-        model.addAttribute("jobs", jobData.findById(id));
+        model.addAttribute("job", jobData.findById(id));
         return "job-detail";
     }
 
@@ -41,8 +43,24 @@ public class JobController {
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
 
+        if (errors.hasErrors()){
+            return "new-job";
+        }
+        else{
 
-        return "";
+            Job newJob = new Job(
+                    jobForm.getName(),
+                    jobData.getEmployers().findById(jobForm.getEmployerId()),
+                    jobData.getLocations().findById(jobForm.getLocationId()),
+                    jobData.getPositionTypes().findById(jobForm.getPositionTypeId()),
+                    jobData.getCoreCompetencies().findById(jobForm.getCoreCompetencyId())
+                    );
+             jobData.add(newJob);
 
+             model.addAttribute("job", newJob);
+
+        return "redirect:/job?id=" + newJob.getId();
+        }
     }
 }
+
